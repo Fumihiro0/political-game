b8698319b1f44ce458fce1b2f04f94367f64e4df
AxizY <24615015+AxizY@users.noreply.github.com>
2021-02-10 12:53:26 -0500

    Update library.js

--- library.js
+++ library.js
@@ -1,1 +1,7 @@
-
+function createButton(value) {
+  var button = document.createElement("BUTTON");
+  button.innerHTML = value;
+  document.getElementById("buttonchoices").appendChild(button);
+}
+function changeImage(value) {document.getElementById("textimage").innerHTML = value}
+function changeText(value) {document.getElementById("textmessage").innerHTML = value}
