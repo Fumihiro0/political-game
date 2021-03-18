ba377b337a3101d6fd00b74d56bb0e786cc7853e
AxizY <24615015+AxizY@users.noreply.github.com>
2021-03-18 18:38:38 -0400

    Update library.js

--- library.js
+++ library.js
@@ -11,9 +11,7 @@ function createButton(value, leadto) {
   document.getElementById("buttonchoices").appendChild(button);
   button.onclick = function()
   {
-    //343345
-    console.log('https://github.com/Starlight-Developers/political-game/tree/main/text-images/'+leadto)
-    fetch('https://github.com/Starlight-Developers/political-game/tree/main/text-images/'+leadto)
+    fetch('https://raw.githubusercontent.com/Starlight-Developers/political-game/main/text-images/'+leadto)
     .then(response => response.text())
     .then(text => changeImage(text))
   };
