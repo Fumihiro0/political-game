80d6a31b2185a2fb1f3715af961d8ca522162e78
AxizY <24615015+AxizY@users.noreply.github.com>
2021-03-20 16:07:37 -0400

    Update library.js

--- library.js
+++ library.js
@@ -4,7 +4,11 @@ var karma = 0;
 
 function changeImage(value) {document.getElementById("image").src = value}
 function changeText(value) {document.getElementById("textmessage").innerHTML = value}
-
+function removeAllChildNodes(parent) { // totally wrote this myself
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 function createButton(value, leadto) {
   var button = document.createElement("BUTTON");
   button.innerHTML = value;
@@ -15,8 +19,6 @@ function createButton(value, leadto) {
     .then(response => response.text())
     .then(text => eval(text))
     changeImage("images/"+leadto+".png");
-    while (document.getElementById("buttonchoices").firstChild) {
-      document.getElementById("buttonchoices").removeChild(document.getElementById("buttonchoices").firstChild);
-    }
+    removeAllChildNodes(document.querySelector("#buttonchoices"));
   };
 }
