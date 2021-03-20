146ff1de6a63c17d31f12eac42486bac6e6b5548
AxizY <24615015+AxizY@users.noreply.github.com>
2021-03-20 17:16:10 -0400

    Update intro.js

--- code-run/intro.js
+++ code-run/intro.js
@@ -1,3 +1,3 @@
-changeText("ur mom XDD")
+changeText("Hello, you are currently in a race to run the country.")
 createButton("Give Money", "gm1", false)
 createButton("Give Reputation", "gr1", false)
