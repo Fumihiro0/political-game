19c36b32f948ad739fa61662c91ed94d9e17a42e
AxizY <24615015+AxizY@users.noreply.github.com>
2021-03-20 16:48:12 -0400

    Update library.js

--- library.js
+++ library.js
@@ -15,7 +15,7 @@ function createButton(value, leadto, changeImageBool) {
   document.getElementById("buttonchoices").appendChild(button);
   button.onclick = function()
   {
-     fetch('main/code-run/'+leadto+'.js')
+     fetch('code-run/'+leadto+'.js')
     .then(response => response.text())
     .then(text => eval(text))
     if(changeImageBool) {changeImage("images/"+leadto+".png");}
